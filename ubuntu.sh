cf2087e5e591cd89a89b968df42a0c611a8e9836
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-01 18:36:38 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -33,6 +33,6 @@ else
 fi
 cd ../
 rm -fr /sub-web/src/views/Subconverter.vue
-curl -fsSL https://raw.githubusercontent.com/281677160/agent/main/Subconverter.vue /sub-web/src/views/Subconverter.vue
+curl -fsSL https://raw.githubusercontent.com/281677160/agent/main/Subconverter.vue > /sub-web/src/views/Subconverter.vue
 yarn build
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh

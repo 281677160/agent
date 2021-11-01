4a6145daae796cbb9d02351f1bb3164410bbef2d
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-01 18:26:43 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,21 +4,21 @@ apt-get install -y curl wget sudo git
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 sudo apt-get install -y nodejs
 if [[ `node --version |egrep -o "v[0-9]+\.[0-9]+\.[0-9]+"` ]]; then
-  echo ""
+	echo "node安装成功!"
 else
 	echo "node安装不成功!"
   exit 1
 fi
 apt install -y npm
 if [[ `npm --version |egrep -o "[0-9]+\.[0-9]+\.[0-9]+"` ]]; then
-  echo ""
+	echo "npm安装成功!"
 else
 	echo "npm安装不成功!"
   exit 1
 fi
 npm install -g yarn
 if [[ `yarn --version |egrep -o "[0-9]+\.[0-9]+\.[0-9]+"` ]]; then
-  echo ""
+	echo "yarn安装成功!"
 else
 	echo "yarn安装不成功!"
   exit 1
@@ -27,9 +27,10 @@ git clone https://github.com/CareyWang/sub-web.git
 if [[ $? -ne 0 ]];then
 	echo "sub-web下载失败!"
 else
-  cd sub-web
-  yarn install
-  yarn serve
+	echo "sub-web下载成功!"
+	cd sub-web
+	yarn install
+	yarn serve
 fi
 cd ../
 rm -fr /sub-web/src/views/Subconverter.vue

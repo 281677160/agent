f1ef38875caf7aaab917a0d73e344e0d572fc82b
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-02 03:15:59 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo
-echo -e “\033[34m 请输入您的前端网页域名[比如：sub.v2rayssr.com] \033[0m”
+echo -e "\033[34m 请输入您的前端网页域名[比如：sub.v2rayssr.com] \033[0m"
 read -p " 请输入您的前端网址域名：" wzym
 export wzym=${wzym}
-echo -e “\033[32m 您的前端网址域名为：${wzym} \033[0m”
+echo -e "\033[32m 您的前端网址域名为：${wzym} \033[0m"
 echo
 echo
-echo -e “\033[34m 请输入您的后端服务地址域名[比如：suc.v2rayssr.com] \033[0m”
+echo -e "\033[34m 请输入您的后端服务地址域名[比如：suc.v2rayssr.com] \033[0m"
 read -p "请输入您的后端服务地址域名：" fwym
 export fwym=${fwym}
-echo -e “\033[32m 您的后台地址为：${fwym} \033[0m”
+echo -e "\033[32m 您的后台地址为：${fwym} \033[0m"
 echo
 sleep 20
 cat >/root/sub_suc <<-EOF

191b06d1036aa804e5d2353a768c0a74c1f35364
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-02 10:07:24 +0800

    Update ubuntu2.sh

--- ubuntu2.sh
+++ ubuntu2.sh
@@ -4,7 +4,7 @@ source /root/sub_suc
 rm -fr /root/sub-web/src/views/Subconverter.vue
 curl -fsSL https://raw.githubusercontent.com/281677160/agent/main/Subconverter.vue > /root/sub-web/src/views/Subconverter.vue
 if [[ $? -ne 0 ]];then
-  echo "文件下载不失败"
+  echo -e "\033[31m Subconverter.vue文件下载失败! \033[0m"
   exit 1
 else
   sed -i "s/192.168.1.1/${fwym}/g" /root/sub-web/src/views/Subconverter.vue
@@ -13,7 +13,7 @@ cd sub-web && yarn build
 cd /root
 echo
 echo
-echo "开始安装宝塔面板，看到提示按 N/Y 的按Y回车继续进行安装"
+echo -e "\033[32m 开始安装宝塔面板，看到提示按 N/Y 的按Y回车继续进行安装! \033[0m"
 echo
 echo
 sleep 10

fd389b7feb101fd60edb95d7adf6ed11b255491b
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-02 11:44:15 +0800

    Update clashzh2.sh

--- clashzh2.sh
+++ clashzh2.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 cd /root
-source /root/sub_suc
-rm -fr /root/sub-web/src/views/Subconverter.vue
-curl -fsSL https://raw.githubusercontent.com/281677160/agent/main/Subconverter.vue > /root/sub-web/src/views/Subconverter.vue
-if [[ $? -ne 0 ]];then
-  echo -e "\033[31m Subconverter.vue文件下载失败! \033[0m"
-  exit 1
-else
-  sed -i "s/192.168.1.1/${fwym}/g" /root/sub-web/src/views/Subconverter.vue
+if [[ -d /root/sub-web ]]; then
+	source /root/sub_suc
+	rm -fr /root/sub-web/src/views/Subconverter.vue
+	curl -fsSL https://raw.githubusercontent.com/281677160/agent/main/Subconverter.vue > /root/sub-web/src/views/Subconverter.vue
+	if [[ $? -ne 0 ]];then
+  	echo -e "\033[31m Subconverter.vue文件下载失败! \033[0m"
+  	exit 1
+	else
+  	sed -i "s/192.168.1.1/${fwym}/g" /root/sub-web/src/views/Subconverter.vue
+	fi
+	cd sub-web && yarn build
+	cd /root
 fi
-cd sub-web && yarn build
-cd /root
 echo
 echo
 echo -e "\033[32m 开始安装宝塔面板，看到提示按 N/Y 的时候按 Y 回车继续进行安装! \033[0m"

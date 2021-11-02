939fd9972692975f771de78fe13508f9b8ba9882
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-02 13:33:54 +0800

    Update converter.vue

--- converter.vue
+++ converter.vue
@@ -36,7 +36,7 @@
                     style="width: 100%"
                     v-model="form.customBackend"
                     :fetch-suggestions="backendSearch"
-                    placeholder="动动小手，（建议）自行搭建后端服务。例：http://127.0.0.1:25500/sub?"
+                    placeholder="动动小手，（建议）自行搭建后端服务。例：http://127.0.0.1/sub?"
                   >
                     <el-button slot="append" @click="gotoGayhub" icon="el-icon-link">前往项目仓库</el-button>
                   </el-autocomplete>

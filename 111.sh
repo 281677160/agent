df9f2c67cbe4bd3f9f27c7c85bf0d688f1f325d5
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-04 18:57:12 +0800

    Create 111.sh

---
+++ 111.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+yum install epel-release -y
+yum update -y
+yum install curl tar nginx -y
+firewall-cmd --zone=public --add-port=80/tcp --permanent > /dev/null 2>&1
+firewall-cmd --zone=public --add-port=443/tcp --permanent > /dev/null 2>&1
+firewall-cmd --reload > /dev/null 2>&1
+systemctl restart nginx
+systemctl start nginx
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
+export MSID="$(cat /proc/sys/kernel/random/uuid)"
+export WEBS="$(date +web%d号ck%M%S)"
+export VMTCP="$(date +vme%ds%Hs%S)"
+export VMWS="$(date +vm%Sw%M%Hs)"
+

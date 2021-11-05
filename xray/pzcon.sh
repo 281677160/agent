99e7c36dc77600ba839111ac4e3c9b737c3072fb
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-05 15:10:00 +0800

    Update pzcon.sh

--- xray/pzcon.sh
+++ xray/pzcon.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
 cat >/usr/local/etc/xray/pzcon <<-EOF
 #!/bin/bash
-TIME() {
-[[ -z "$1" ]] && {
-	echo -ne " "
-} || {
-     case $1 in
-	r) export Color="\e[31;1m";;
-	g) export Color="\e[32;1m";;
-	b) export Color="\e[34;1m";;
-	y) export Color="\e[33;1m";;
-	z) export Color="\e[35;1m";;
-	l) export Color="\e[36;1m";;
-      esac
-	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
-		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
-	 }
-      }
-}
+clear
+echo
+echo
 echo -e "\033[31m VLESS+TCP+XTLS \033[0m"
 echo
-TIME g "vless://${MSID}@${getIpAddress}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${wzym}#VLESS+TCP+XTLS"
+echo "vless://${MSID}@${getIpAddress}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${wzym}#VLESS+TCP+XTLS"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TCP+XTLS（可自行修改） \033[0m"
-echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
-echo -e "\033[33m 协议：V2Ray/XRay \033[0m"
+echo -e "\033[33m 节点类型：XRay \033[0m"
+echo -e "\033[33m 协议：VLESS \033[0m"
 echo -e "\033[33m 服务器地址：${getIpAddress} \033[0m"
 echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 加密方式：none \033[0m"
 echo -e "\033[33m UUID：${MSID} \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"
 echo -e "\033[33m XTLS：勾选 \033[0m"
-echo -e "\033[33m flow：xtls-rprx-direct \033[0m"
+echo -e "\033[33m 流控(flow)：xtls-rprx-direct \033[0m"
 echo -e "\033[33m alpn：默认 \033[0m"
 echo -e "\033[33m 域名：${wzym} \033[0m"
 echo -e "\033[33m 传输协议：TCP \033[0m"
@@ -42,11 +28,11 @@ echo
 echo
 echo -e "\033[31m VLESS+TPC+TLS \033[0m"
 echo
-TIME g "vless://${MSID}@${wzym}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${wzym}#VLESS+TPC+TLS"
+echo "vless://${MSID}@${wzym}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${wzym}#VLESS+TPC+TLS"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS（可自行修改） \033[0m"
-echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
-echo -e "\033[33m 协议：V2Ray/XRay \033[0m"
+echo -e "\033[33m 节点类型：XRay \033[0m"
+echo -e "\033[33m 协议：VLESS \033[0m"
 echo -e "\033[33m 服务器地址：${wzym} \033[0m"
 echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 加密方式：none \033[0m"
@@ -63,11 +49,11 @@ echo
 echo
 echo -e "\033[31m VLESS+WS+TLS \033[0m"
 echo
-TIME g "vless://${MSID}@${wzym}:${PORT}?host=&path=%2F${WEBS}&type=ws&encryption=none&security=tls&sni=${wzym}#VLESS+WS+TLS"
+echo "vless://${MSID}@${wzym}:${PORT}?host=&path=%2F${WEBS}&type=ws&encryption=none&security=tls&sni=${wzym}#VLESS+WS+TLS"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS（可自行修改） \033[0m"
-echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
-echo -e "\033[33m 协议：V2Ray/XRay \033[0m"
+echo -e "\033[33m 节点类型：XRay \033[0m"
+echo -e "\033[33m 协议：VLESS \033[0m"
 echo -e "\033[33m 服务器地址：${wzym} \033[0m"
 echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 加密方式：none \033[0m"
@@ -78,5 +64,5 @@ echo -e "\033[33m alpn：默认 \033[0m"
 echo -e "\033[33m 域名：${wzym} \033[0m"
 echo -e "\033[33m 传输协议：WebSocket \033[0m"
 echo -e "\033[33m WebSocket Host：空 \033[0m"
-echo -e "\033[33m WebSocket Path：${WEBS} \033[0m"
+echo -e "\033[33m WebSocket Path：/${WEBS} \033[0m"
 EOF

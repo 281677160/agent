8752e006f606f3053bfab0162c2441a53bb253de
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-05 12:33:17 +0800

    Update pzcon.sh

--- xray/pzcon.sh
+++ xray/pzcon.sh
@@ -2,13 +2,13 @@
 cat >/usr/local/etc/xray/pzcon <<-EOF
 echo -e "\033[31m VLESS+TCP+XTLS \033[0m"
 echo
-echo -e "\033[32m vless://${MSID}@${getIpAddress}:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${YUMING}#VLESS+TCP+XTLS \033[0m"
+echo -e "\033[32m vless://${MSID}@${getIpAddress}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${YUMING}#VLESS+TCP+XTLS \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TCP+XTLS（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
 echo -e "\033[33m 协议：V2Ray/XRay \033[0m"
 echo -e "\033[33m 服务器地址：${getIpAddress} \033[0m"
-echo -e "\033[33m 服务器端口：443 \033[0m"
+echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 加密方式：none \033[0m"
 echo -e "\033[33m UUID：${MSID} \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"
@@ -24,13 +24,13 @@ echo
 echo
 echo -e "\033[31m VLESS+TPC+TLS \033[0m"
 echo
-echo -e "\033[32m vless://${MSID}@${YUMING}:443?headerType=none&type=tcp&encryption=none&security=tls&sni=${YUMING}#VLESS+TPC+TLS \033[0m"
+echo -e "\033[32m vless://${MSID}@${YUMING}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${YUMING}#VLESS+TPC+TLS \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
 echo -e "\033[33m 协议：V2Ray/XRay \033[0m"
 echo -e "\033[33m 服务器地址：${YUMING} \033[0m"
-echo -e "\033[33m 服务器端口：443 \033[0m"
+echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 加密方式：none \033[0m"
 echo -e "\033[33m UUID：${MSID} \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"
@@ -45,13 +45,13 @@ echo
 echo
 echo -e "\033[31m VLESS+WS+TLS \033[0m"
 echo
-echo -e "\033[32m vless://${MSID}@${YUMING}:443?host=&path=%2F${WEBS}&type=ws&encryption=none&security=tls&sni=${YUMING}#VLESS+WS+TLS \033[0m"
+echo -e "\033[32m vless://${MSID}@${YUMING}:${PORT}?host=&path=%2F${WEBS}&type=ws&encryption=none&security=tls&sni=${YUMING}#VLESS+WS+TLS \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
 echo -e "\033[33m 协议：V2Ray/XRay \033[0m"
 echo -e "\033[33m 服务器地址：${YUMING} \033[0m"
-echo -e "\033[33m 服务器端口：443 \033[0m"
+echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 加密方式：none \033[0m"
 echo -e "\033[33m UUID：${MSID} \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"

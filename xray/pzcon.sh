d67a9850d7d0e6382700adafb77bbe861123c3be
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-05 14:57:00 +0800

    Update pzcon.sh

--- xray/pzcon.sh
+++ xray/pzcon.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 cat >/usr/local/etc/xray/pzcon <<-EOF
+#!/bin/bash
+TIME() {
+[[ -z "$1" ]] && {
+	echo -ne " "
+} || {
+     case $1 in
+	r) export Color="\e[31;1m";;
+	g) export Color="\e[32;1m";;
+	b) export Color="\e[34;1m";;
+	y) export Color="\e[33;1m";;
+	z) export Color="\e[35;1m";;
+	l) export Color="\e[36;1m";;
+      esac
+	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+	 }
+      }
+}
 echo -e "\033[31m VLESS+TCP+XTLS \033[0m"
 echo
-echo -e "\033[32m vless://${MSID}@${getIpAddress}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${wzym}#VLESS+TCP+XTLS \033[0m"
+TIME g "vless://${MSID}@${getIpAddress}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${wzym}#VLESS+TCP+XTLS"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TCP+XTLS（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
@@ -24,7 +42,7 @@ echo
 echo
 echo -e "\033[31m VLESS+TPC+TLS \033[0m"
 echo
-echo -e "\033[32m vless://${MSID}@${wzym}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${wzym}#VLESS+TPC+TLS \033[0m"
+TIME g "vless://${MSID}@${wzym}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${wzym}#VLESS+TPC+TLS"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"
@@ -45,7 +63,7 @@ echo
 echo
 echo -e "\033[31m VLESS+WS+TLS \033[0m"
 echo
-echo -e "\033[32m vless://${MSID}@${wzym}:${PORT}?host=&path=%2F${WEBS}&type=ws&encryption=none&security=tls&sni=${wzym}#VLESS+WS+TLS \033[0m"
+TIME g "vless://${MSID}@${wzym}:${PORT}?host=&path=%2F${WEBS}&type=ws&encryption=none&security=tls&sni=${wzym}#VLESS+WS+TLS"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：V2Ray/XRay \033[0m"

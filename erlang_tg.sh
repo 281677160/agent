f95013773599b5f32bc0292f0532f8b2ed90748a
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-17 21:19:16 +0800

    Update erlang_tg.sh

--- erlang_tg.sh
+++ erlang_tg.sh
@@ -145,8 +145,6 @@ kaishi_install() {
     echo -e "\033[33m 正在为您安装TG代理，请稍后... \033[0m"
     sys_pro="/etc/systemd/system"
     sleep 1
-    [[ ! -d ${sys_pro} ]] && mkdir -p ${sys_pro}
-    do_systemd_system
 }
 
 do_configure_os() {

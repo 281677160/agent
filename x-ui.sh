8ee354db40bb2a4de9b4cb6a1825796ce53b110b
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-18 15:22:13 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -310,7 +310,17 @@ function port_exist_check() {
 
 function xui_install() {
   print_ok "安装 x-ui"
-  bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+  wget -q -P /root https://ghproxy.com/https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz -O /root/x-ui-linux-amd64.tar.gz
+  rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+  tar zxvf x-ui-linux-amd64.tar.gz
+  chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+  cp x-ui/x-ui.sh /usr/bin/x-ui
+  cp -f x-ui/x-ui.service /etc/systemd/system/
+  mv x-ui/ /usr/local/
+  systemctl daemon-reload
+  systemctl enable x-ui
+  systemctl restart x-ui
+  rm -rf /root/x-ui-linux-amd64.tar.gz
   judge "x-ui 安装"
 }
 

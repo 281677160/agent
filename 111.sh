64482bd77a4b610759f65a51943d3e951e6f7c98
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-01 13:21:39 +0800

    Update 111.sh

--- 111.sh
+++ 111.sh
@@ -86,7 +86,7 @@ function running_state() {
 	sudo apt-get update -y
 	sudo apt-get full-upgrade -y
 	sudo apt-get install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
-	judge "安装/升级必要依赖"
+	judge "部署编译环境"
 	sudo timedatectl set-timezone Asia/Shanghai
 }
 

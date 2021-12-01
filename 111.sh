0a815d7780f62b1b2949ce0e06f55ce213c1ab68
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-01 16:43:11 +0800

    Update 111.sh

--- 111.sh
+++ 111.sh
@@ -186,7 +186,8 @@ function nginx_install() {
 ECHOG "正在下载源码中,请耐心等候~~~"
 if [[ $firmware == "Lede_source" ]]; then
 	if [[ -d ${Home} ]]; then
-		shopt -s extglob && cd openwrt
+		cd openwrt
+		shopt -s extglob
 		rm -fr !(toolchain|tools|config_bf|build_dir|staging_dir)
 		cd ../
 		git clone https://github.com/coolsnowwolf/lede ledeopen

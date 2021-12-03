3453a43815efa9cb42de0b55cf440182257ac322
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-03 13:52:53 +0800

    Update 222.sh

--- 222.sh
+++ 222.sh
@@ -249,7 +249,7 @@ function amlogic_s9xxx() {
   if [[ "${firmware}" == "openwrt_amlogic" ]]; then
     ECHOY "正在下载打包所需的内核,请耐心等候~~~"
     if [[ -d amlogic/amlogic-s9xxx ]]; then
-      ECHOG "发现老旧内核存在，请输入ubuntu密码删除老旧内核"
+      ECHOGG "发现老旧内核存在，请输入ubuntu密码删除老旧内核"
       sudo rm -rf amlogic
     fi
     mkdir -p amlogic

99ec3b1bff684c1f1a695a4a8fafac4424474c3e
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-07 17:19:48 +0800

    Update 333.sh

--- 333.sh
+++ 333.sh
@@ -144,7 +144,7 @@ function op_kongjian() {
 function op_diywenjian() {
   cd ${GITHUB_WORKSPACE}
   if [[ ! -d ${GITHUB_WORKSPACE}/OP_DIY ]]; then
-    git clone https://github.com/281677160/bendi
+    rm -rf bendi && git clone https://github.com/281677160/bendi
     judgeopen "OP_DIY文件下载"
     cp -Rf ${GITHUB_WORKSPACE}/bendi/OP_DIY ${GITHUB_WORKSPACE}/OP_DIY
   fi

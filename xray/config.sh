41722978863d511c28cece64fac7bcfca41387f0
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-16 16:50:56 +0800

    Update config.sh

--- xray/config.sh
+++ xray/config.sh
@@ -1,90 +1,166 @@
 #!/bin/bash
 cat >/usr/local/etc/xray/config.json <<-EOF
 {
-  "log": {
-    "loglevel": "warning"
-  },
-  "inbounds": [
-    {
-      "port": ${PORT},
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "${UUID}",
-            "flow": "xtls-rprx-direct"
-          }
-        ],
-        "decryption": "none",
-        "fallbacks": [
-          {
-            "dest": 60000,
-            "alpn": "",
-            "xver": 1
-          },
-          {
-            "dest": 60001,
-            "alpn": "h2",
-            "xver": 1
-          },
-          {
-            "dest": 60002,
-            "path": "/${WS_PATH}/",
-            "xver": 1
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "tcp",
-        "security": "xtls",
-        "xtlsSettings": {
-          "minVersion": "1.2",
-          "certificates": [
-            {
-              "certificateFile": "/usr/local/etc/xray/self_signed_cert.pem",
-              "keyFile": "/usr/local/etc/xray/self_signed_key.pem"
-            },
-            {
-              "certificateFile": "/ssl/xray.crt",
-              "keyFile": "/ssl/xray.key"
-            }
-          ]
-        }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-      }
+    "log": {
+        "loglevel": "warning"
     },
-    {
-      "port": 60002,
-      "listen": "127.0.0.1",
-      "protocol": "vless",
-      "settings": {
-          "clients": [
-              {
-                  "id": "${UUID}"
-              }
-          ],
-          "decryption": "none"
-      },
-      "streamSettings": {
-          "network": "ws",
-          "security": "none",
-          "wsSettings": {
-              "acceptProxyProtocol": true,
-              "path": "/${WS_PATH}/"
-          }
-      }
-  }
-],
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    }
-  ]
+    "inbounds": [
+        {
+            "port": ${PORT},
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "flow": "xtls-rprx-direct",
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 52000,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/${WS_PATH}/",
+                        "dest": 52001,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/${VMTCP}/",
+                        "dest": 52002,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/${VMWS}/",
+                        "dest": 52003,
+                        "xver": 1
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "xtls",
+                "xtlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/ssl/xray.crt",
+                            "keyFile": "/ssl/xray.key"
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "port": 52000,
+            "listen": "127.0.0.1",
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "${QJPASS}",
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ],
+                "fallbacks": [
+                    {
+                        "dest": 80
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true
+                }
+            }
+        },
+        {
+            "port": 52001,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true,
+                    "path": "/${WS_PATH}/"
+                }
+            }
+        },
+        {
+            "port": 52002,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true,
+                    "header": {
+                        "type": "http",
+                        "request": {
+                            "path": [
+                                "/${VMTCP}/"
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "port": 52003,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true,
+                    "path": "/${VMWS}/"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF

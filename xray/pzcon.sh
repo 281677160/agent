41a382d43838a8b3d203de7dc24c0c13f2d865a4
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-18 02:16:25 +0800

    Update pzcon.sh

--- xray/pzcon.sh
+++ xray/pzcon.sh
@@ -7,31 +7,29 @@ echo
 echo -e "\033[41;33m 信息查询于：$(date +%Y年%m月%d号%H时%M分%S秒)  \033[0m"
 echo
 echo
-echo -e "\033[31m （1）Trojan \033[0m"
+echo -e "\033[31m （1）Trojan-Plus \033[0m"
 echo
-echo "trojan://${QJPASS}@${domain}:${PORT}/?sni=${domain}&tls=1&allowinsecure=1#trojan+${domain}"
+echo "trojan://${QJPASS}@${domain}:${PORT}/?sni=${domain}&tls=1&allowinsecure=1#trojan_${domain}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=trojan://${QJPASS}@${domain}:${PORT}/?sni=${domain}&tls=1&allowinsecure=1#trojan+${domain} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=trojan://${QJPASS}@${domain}:${PORT}/?sni=${domain}&tls=1&allowinsecure=1#trojan_${domain} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：Trojan+${PORT}（可自行修改） \033[0m"
-echo -e "\033[33m 节点类型：XRay \033[0m"
-echo -e "\033[33m 协议：Trojan \033[0m"
-echo -e "\033[33m 服务器地址：${domain} \033[0m"
+echo -e "\033[33m 节点类型：Trojan-Plus \033[0m"
+echo -e "\033[33m 地址（支持域名）：${domain} \033[0m"
 echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m 密码：${QJPASS} \033[0m"
-echo -e "\033[33m 传输协议：TCP \033[0m"
-echo -e "\033[33m 伪装协议：未配置/none \033[0m"
+echo -e "\033[33m TCP快速打开：false \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"
-echo -e "\033[33m 指纹伪造：禁用 \033[0m"
-echo -e "\033[33m TLS Host：空 \033[0m"
+echo -e "\033[33m Session Ticket：不勾选 \033[0m"
+echo -e "\033[33m 域名：${domain} \033[0m"
 echo
 echo
 echo
 echo -e "\033[31m （2）VLESS+TCP+XTLS \033[0m"
 echo
-echo "vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${domain}#VLESS+TCP+XTLS+${domain}"
+echo "vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${domain}#VLESS_TCP_XTLS_${domain}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain_ip}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${domain}#VLESS+TCP+XTLS+${domain} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=${domain}#VLESS_TCP_XTLS_${domain} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TCP+XTLS${PORT}（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：XRay \033[0m"
@@ -46,15 +44,15 @@ echo -e "\033[33m 流控(flow)：xtls-rprx-direct \033[0m"
 echo -e "\033[33m alpn：默认 \033[0m"
 echo -e "\033[33m 域名：${domain} \033[0m"
 echo -e "\033[33m 传输协议：TCP \033[0m"
-echo -e "\033[33m 伪装协议：未配置/none \033[0m"
+echo -e "\033[33m 伪装协议：none/none \033[0m"
 echo
 echo
 echo
 echo -e "\033[31m （3）VLESS+TPC+TLS \033[0m"
 echo
-echo "vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${domain}#VLESS+TPC+TLS+${PORT}"
+echo "vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${domain}#VLESS_TPC_TLS_${domain}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${domain}#VLESS+TPC+TLS+${PORT} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?headerType=none&type=tcp&encryption=none&security=tls&sni=${domain}#VLESS_TPC_TLS_${domain} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TPC+TLS${PORT}（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：XRay \033[0m"
@@ -68,7 +66,7 @@ echo -e "\033[33m XTLS：不选 \033[0m"
 echo -e "\033[33m alpn：默认 \033[0m"
 echo -e "\033[33m 域名：${domain} \033[0m"
 echo -e "\033[33m 传输协议：TCP \033[0m"
-echo -e "\033[33m 伪装协议：未配置/none \033[0m"
+echo -e "\033[33m 伪装协议：none/none \033[0m"
 echo
 echo
 echo

095549c58030a6ff4ae0bb709d749687aafa692f
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-07 02:40:05 +0800

    Update xray_install.sh

--- xray_install.sh
+++ xray_install.sh
@@ -166,7 +166,6 @@ function system_check() {
   systemctl disable nftables
   systemctl stop ufw
   systemctl disable ufw
-  service iptables stop
   if [[ "${XITONG_ID}" == "ubuntu" ]] || [[ "${XITONG_ID}" == "debian" ]]; then
     ufw disable
     apt-get remove -y iptables

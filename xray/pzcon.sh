4654c5ec484e30e8d730c97adda0870be7f1ea0c
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-08 18:05:07 +0800

    Update pzcon.sh

--- xray/pzcon.sh
+++ xray/pzcon.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-qrCodeBase64Default=$(echo -n "{\"ps\":\"${domain}-WS7\"}" | base64 -w 0)
-qrCodeBase64Default="${qrCodeBase64Default}"
-TCPqrCodeBase64Default=$(echo -n "{\"add\":\"${domain}\",\"aid\":0,\"host\":\"${domain}\",\"id\":\"${UUID}\",\"net\":\"tcp\",\"path\":\"/${VMESS_TCP_PATH}/\",\"port\":${PORT},\"ps\":\"VMESS_TCP_TLS_${domain}\",\"scy\":\"none\",\"sni\":\"${domain}\",\"tls\":\"tls\",\"v\":2,\"type\":\"http\",\"allowInsecure\":0,\"peer\":\"${domain}\",\"obfs\":\"http\",\"obfsParam\":\"${domain}\"}" | base64)
-TCPqrCodeBase64Default="${TCPqrCodeBase64Default}"
+tcpBase64Default=$(echo -n "{\"add\":\"${domain}\",\"aid\":0,\"host\":\"${domain}\",\"id\":\"${UUID}\",\"net\":\"tcp\",\"path\":\"/${VMESS_TCP_PATH}/\",\"port\":${PORT},\"ps\":\"VMESS_TCP_TLS_${domain}\",\"scy\":\"none\",\"sni\":\"${domain}\",\"tls\":\"tls\",\"v\":2,\"type\":\"http\",\"allowInsecure\":0,\"peer\":\"${domain}\",\"obfs\":\"http\",\"obfsParam\":\"${domain}\"}" | base64)
+Vmess_tcp="${tcpBase64Default// /}"
+
+wsBase64Default=$(echo -n "{\"port\":${PORT},\"ps\":\"VMESS_WS_TLS_${domain}\",\"tls\":\"tls\",\"id\":\"${UUID}\",\"aid\":0,\"v\":2,\"host\":\"${domain}\",\"type\":\"none\",\"path\":\"/${VMESS_WS_PATH}/\",\"net\":\"ws\",\"add\":\"${domain}\",\"allowInsecure\":0,\"method\":\"none\",\"peer\":\"${domain}\",\"sni\":\"${domain}\"}" | base64 -w 0)
+Vmess_ws="${wsBase64Default// /}"
+
 cat >/usr/local/etc/xray/pzcon <<-EOF
 #!/bin/bash
 # 此文件请勿删除和做任何修改
@@ -75,7 +77,7 @@ echo -e "\033[33m 伪装协议：none/none \033[0m"
 echo
 echo
 echo
-echo -e "\033[31m （4）VLESS+WS+TLS（可套CF使用） \033[0m"
+echo -e "\033[31m （4）VLESS+WS+TLS（可套cloudflare CDN使用） \033[0m"
 echo
 echo "vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${VLESS_WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain}"
 echo
@@ -100,9 +102,9 @@ echo
 echo
 echo -e "\033[31m （5）VMESS+TCP+TLS \033[0m"
 echo
-echo "vmess://${TCPqrCodeBase64Default}"
+echo "vmess://${Vmess_tcp}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vmess://${TCPqrCodeBase64Default} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vmess://${Vmess_tcp} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+TCP+TLS${PORT}（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：XRay \033[0m"
@@ -121,11 +123,11 @@ echo -e "\033[33m TLS Host：${domain} \033[0m"
 echo
 echo
 echo
-echo -e "\033[31m （6）VMESS+WS+TLS（可套CF使用） \033[0m"
+echo -e "\033[31m （6）VMESS+WS+TLS（可套cloudflare CDN使用） \033[0m"
 echo
-echo "vmess://${qrCodeBase64Default}"
+echo "vmess://${Vmess_ws}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vmess://${qrCodeBase64Default} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vmess://${Vmess_ws} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+WS+TLS${PORT}（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：XRay \033[0m"

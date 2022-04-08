94f8748537dd7d33fa474540af58677fb4ab383a
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-08 14:59:52 +0800

    Update pzcon.sh

--- xray/pzcon.sh
+++ xray/pzcon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-qrCodeBase64Default=$(echo -n "{\"port\":${PORT},\"ps\":\"VMESS_WS_TLS_${domain}\",\"tls\":\"tls\",\"id\":\"${UUID}\",\"aid\":0,\"v\":2,\"host\":\"${domain}\",\"type\":\"none\",\"path\":\"/${VMWS}/\",\"net\":\"ws\",\"add\":\"${domain}\",\"allowInsecure\":0,\"method\":\"none\",\"peer\":\"${domain}\",\"sni\":\"${domain}\"}" | base64 -w 0)
+qrCodeBase64Default=$(echo -n "{\"port\":${PORT},\"ps\":\"VMESS_WS_TLS_${domain}\",\"tls\":\"tls\",\"id\":\"${UUID}\",\"aid\":0,\"v\":2,\"host\":\"${domain}\",\"type\":\"none\",\"path\":\"/${VMESS_WS_PATH}/\",\"net\":\"ws\",\"add\":\"${domain}\",\"allowInsecure\":0,\"method\":\"none\",\"peer\":\"${domain}\",\"sni\":\"${domain}\"}" | base64 -w 0)
 qrCodeBase64Default="${qrCodeBase64Default// /}"
-TCPqrCodeBase64Default=$(echo -n "{\"add\":\"${domain}\",\"aid\":0,\"host\":\"${domain}\",\"id\":\"${UUID}\",\"net\":\"tcp\",\"path\":\"/${VMTCP}/\",\"port\":${PORT},\"ps\":\"VMESS_TCP_TLS_${domain}\",\"scy\":\"none\",\"sni\":\"${domain}\",\"tls\":\"tls\",\"v\":2,\"type\":\"http\",\"allowInsecure\":0,\"peer\":\"${domain}\",\"obfs\":\"http\",\"obfsParam\":\"${domain}\"}" | base64)
+TCPqrCodeBase64Default=$(echo -n "{\"add\":\"${domain}\",\"aid\":0,\"host\":\"${domain}\",\"id\":\"${UUID}\",\"net\":\"tcp\",\"path\":\"/${VMESS_TCP_PATH}/\",\"port\":${PORT},\"ps\":\"VMESS_TCP_TLS_${domain}\",\"scy\":\"none\",\"sni\":\"${domain}\",\"tls\":\"tls\",\"v\":2,\"type\":\"http\",\"allowInsecure\":0,\"peer\":\"${domain}\",\"obfs\":\"http\",\"obfsParam\":\"${domain}\"}" | base64)
 TCPqrCodeBase64Default="${TCPqrCodeBase64Default// /}"
 cat >/usr/local/etc/xray/pzcon <<-EOF
 #!/bin/bash
@@ -77,9 +77,9 @@ echo
 echo
 echo -e "\033[31m （4）VLESS+WS+TLS（可套CF使用） \033[0m"
 echo
-echo "vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain}"
+echo "vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${VLESS_WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${VLESS_WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+WS+TLS${PORT}（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：XRay \033[0m"
@@ -94,7 +94,7 @@ echo -e "\033[33m alpn：默认 \033[0m"
 echo -e "\033[33m 域名：${domain} \033[0m"
 echo -e "\033[33m 传输协议：WebSocket/ws \033[0m"
 echo -e "\033[33m WebSocket Host：${domain} \033[0m"
-echo -e "\033[33m WebSocket Path：/${WS_PATH}/ \033[0m"
+echo -e "\033[33m WebSocket Path：/${VLESS_WS_PATH}/ \033[0m"
 echo
 echo
 echo
@@ -114,7 +114,7 @@ echo -e "\033[33m 加密方式：AUTO \033[0m"
 echo -e "\033[33m 传输协议：TCP \033[0m"
 echo -e "\033[33m 伪装类型：HTTP \033[0m"
 echo -e "\033[33m HTTP Host：${domain} \033[0m"
-echo -e "\033[33m HTTP Path：/${VMTCP}/ \033[0m"
+echo -e "\033[33m HTTP Path：/${VMESS_TCP_PATH}/ \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"
 echo -e "\033[33m 指纹伪造：禁用 \033[0m"
 echo -e "\033[33m TLS Host：${domain} \033[0m"
@@ -135,7 +135,7 @@ echo -e "\033[33m 服务器端口：${PORT} \033[0m"
 echo -e "\033[33m UUID：${UUID} \033[0m"
 echo -e "\033[33m 加密方式：AUTO \033[0m"
 echo -e "\033[33m 传输协议：WebSocket/ws \033[0m"
-echo -e "\033[33m WebSocket Path：/${VMWS}/ \033[0m"
+echo -e "\033[33m WebSocket Path：/${VMESS_WS_PATH}/ \033[0m"
 echo -e "\033[33m TLS：勾选 \033[0m"
 echo -e "\033[33m 指纹伪造：禁用 \033[0m"
 echo -e "\033[33m TLS Host：${domain} \033[0m"

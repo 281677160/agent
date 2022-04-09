91d801cf669f53d2e5a731669e89e538cc5ab494
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-10 03:19:51 +0800

    Update clash_install.sh

--- clash_install.sh
+++ clash_install.sh
@@ -94,6 +94,7 @@ else
 	sed -i "s?https://suo.yt?http://${wzym}:25500?g" "/root/sub-web/.env"
 	sed -i "s?http://127.0.0.1:25500/sub?http://${wzym}:25500/sub?g" "/root/sub-web/src/views/Subconverter.vue"
 	yarn install
+	timeout -k 1s 50s yarn serve
 	yarn build
 	rm -rf /var/www/html/*
 	cp -R /root/sub-web/dist/* /var/www/html/

5188a3513f815bc92f8a188d1276c887af4b4ad4
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-10 02:33:03 +0800

    Update clash_install.sh

--- clash_install.sh
+++ clash_install.sh
@@ -66,6 +66,25 @@ else
 	echo -e "\033[31m yarn安装失败! \033[0m"
   exit 1
 fi
+
+
+cat >/etc/nginx/sites-available/default <<-EOF
+server {
+listen 80;
+
+server_name localhost;
+
+root /var/www/html/;
+index index.html;
+
+location / {
+    try_files $uri $uri/ =404;
+}
+}
+EOF
+
+systemctl restart nginx
+
 rm -fr sub-web && git clone https://github.com/CareyWang/sub-web.git
 if [[ $? -ne 0 ]];then
 	echo -e "\033[31m sub-web下载失败! \033[0m"
@@ -79,3 +98,30 @@ else
 	rm -rf /var/www/html/*
 	cp -R /root/sub-web/dist/* /var/www/html/
 fi
+cd /root
+wget https://github.com/tindy2013/subconverter/releases/download/v0.7.2/subconverter_linux64.tar.gz
+tar -zxvf subconverter_linux64.tar.gz
+sed -i "s?listen = "0.0.0.0"?listen = "127.0.0.1"?g" "/root/subconverter/pref.toml"
+sed -i "s?listen = "0.0.0.0"?listen = "127.0.0.1"?g" "/root/subconverter/pref.example.ini"
+sed -i "s?http://127.0.0.1:25500/sub?http://${wzym}:25500?g" "/root/subconverter/pref.example.ini"
+
+cat >/etc/systemd/system/sub.service <<-EOF
+[Unit]
+Description=A API For Subscription Convert
+After=network.target
+ 
+[Service]
+Type=simple
+ExecStart=/root/subconverter/subconverter
+WorkingDirectory=/root/subconverter
+Restart=always
+RestartSec=10
+ 
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod +x /etc/systemd/system/sub.service
+systemctl daemon-reload
+systemctl start sub
+systemctl enable sub
+systemctl status sub

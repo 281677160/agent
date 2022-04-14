106147fc9f1b9efda4d79a1382af16c04bb5f216
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-15 02:43:19 +0800

    Update xray_install.sh

--- xray_install.sh
+++ xray_install.sh
@@ -459,6 +459,8 @@ function acme() {
   
   "$HOME"/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 
+  sed -i "6s/^/#/" "$nginx_conf"
+  sed -i "6a\\\troot $website_dir;" "$nginx_conf"
   systemctl restart nginx
 
   if "$HOME"/.acme.sh/acme.sh  --issue  -d ${domain}  --nginx /etc/nginx/conf.d/${domain}.conf -k ec-256 --force; then
@@ -476,6 +478,9 @@ function acme() {
     rm -rf "$HOME/.acme.sh/${domain}_ecc"
     exit 1
   fi
+
+  sed -i "7d" "$nginx_conf"
+  sed -i "6s/#//" "$nginx_conf"
 }
 
 function xrayliugen_conf() {

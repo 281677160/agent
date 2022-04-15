187737c7972fb99984f82b0f7f457b2f008d1e28
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-15 17:52:44 +0800

    Update clash_install.sh

--- clash_install.sh
+++ clash_install.sh
@@ -122,7 +122,7 @@ function nodejs_install() {
     echo "deb http://nginx.org/packages/${PUBKEY} $(lsb_release -cs) nginx" >/etc/apt/sources.list.d/nginx.list
     curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
     apt-get update
-    ${INS} nodejs yarn npm
+    ${INS} nodejs yarn
     npm install pm2 -g
 }
 

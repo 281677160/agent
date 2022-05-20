33a469c96254ef444861cdfaa046a597ecd92eea
281677160 <66651997+281677160@users.noreply.github.com>
2022-05-20 14:23:28 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -651,18 +651,6 @@ server
 	    proxy_set_header X-Real-IP \$remote_addr;
 	    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
-    location /vlpath {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:54512;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
-        proxy_read_timeout 300s;
-        # Show realip in v2ray access.log
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  }
 }
 EOF
   chmod -R 755 /etc/nginx/conf.d
